ced0821a31f8990d2d91db5a23327dbe6e6e8282
nikov <nikovMon@proton.me>
2026-06-25 01:56:42 +0300

    fixing the cpm issue and adding rabbitmq-client lib successfully

---
+++ libs/rabbit-client/RabbitClientDiagnostics.cs
@@ -0,0 +1,55 @@
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+
+namespace ImagingPipeline.RabbitClient;
+
+internal static class RabbitClientDiagnostics
+{
+    public const string ActivitySourceName = "ImagingPipeline.RabbitClient";
+    public const string MeterName = "ImagingPipeline.RabbitClient";
+
+    public static readonly ActivitySource ActivitySource = new(ActivitySourceName);
+
+    private static readonly Meter Meter = new(MeterName);
+
+    public static readonly Counter<long> PublishedMessages =
+        Meter.CreateCounter<long>("imagingpipeline.rabbitmq.messages.published");
+
+    public static readonly Counter<long> PublishFailures =
+        Meter.CreateCounter<long>("imagingpipeline.rabbitmq.messages.publish_failures");
+
+    public static readonly Histogram<double> PublishDurationMs =
+        Meter.CreateHistogram<double>("imagingpipeline.rabbitmq.publish.duration", "ms");
+
+    public static readonly Counter<long> ConsumedMessages =
+        Meter.CreateCounter<long>("imagingpipeline.rabbitmq.messages.consumed");
+
+    public static readonly Counter<long> HandlerFailures =
+        Meter.CreateCounter<long>("imagingpipeline.rabbitmq.messages.handler_failures");
+
+    public static readonly Histogram<double> ProcessingDurationMs =
+        Meter.CreateHistogram<double>("imagingpipeline.rabbitmq.consumer.processing.duration", "ms");
+
+    public static readonly Counter<long> AckedMessages =
+        Meter.CreateCounter<long>("imagingpipeline.rabbitmq.messages.acked");
+
+    public static readonly Counter<long> NackedMessages =
+        Meter.CreateCounter<long>("imagingpipeline.rabbitmq.messages.nacked");
+
+    public static readonly Counter<long> RetriedMessages =
+        Meter.CreateCounter<long>("imagingpipeline.rabbitmq.messages.retried");
+
+    public static readonly Counter<long> ErrorMessages =
+        Meter.CreateCounter<long>("imagingpipeline.rabbitmq.messages.error_routed");
+
+    public static readonly UpDownCounter<long> PublisherChannels =
+        Meter.CreateUpDownCounter<long>("imagingpipeline.rabbitmq.publisher.channels");
+
+    public static readonly Counter<long> ConnectionFailures =
+        Meter.CreateCounter<long>("imagingpipeline.rabbitmq.connection.failures");
+
+    public static readonly Counter<long> ConnectionRecoveries =
+        Meter.CreateCounter<long>("imagingpipeline.rabbitmq.connection.recoveries");
+
+    public static KeyValuePair<string, object?> Tag(string key, object? value) => new(key, value);
+}
