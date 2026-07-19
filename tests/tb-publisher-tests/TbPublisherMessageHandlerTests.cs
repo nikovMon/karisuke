51bd547572793df80cdbf52e1dcb80508022ffad
Shaflinta <kambukka0@proton.me>
2026-07-19 17:45:39 +0300

    refactor(tb-publisher): reorganize Application namespace into MessageHandling

--- tests/tb-publisher-tests/TbPublisherMessageHandlerTests.cs
+++ tests/tb-publisher-tests/TbPublisherMessageHandlerTests.cs
@@ -3,7 +3,7 @@ using System.Text.Json;
 using ImagingPipeline.Common.Dtos.Messaging;
 using ImagingPipeline.ProjectionMapperClient;
 using ImagingPipeline.RabbitMqClient;
-using ImagingPipeline.TbPublisher.Application;
+using ImagingPipeline.TbPublisher.MessageHandling;
 using ImagingPipeline.TbPublisher.Processing;
 using ImagingPipeline.TbPublisher.Tests.Fakes;
 using Microsoft.Extensions.Logging.Abstractions;
