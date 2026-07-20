9111f697a26be6cb1563823762dfb6ad51d08a29
kipodFace <slugBrain@proton.me>
2026-07-20 15:17:32 +0300

    Remove summary comments from AlgorithmName

--- libs/common-dtos/Rules/Models/AlgorithmName.cs
+++ libs/common-dtos/Rules/Models/AlgorithmName.cs
@@ -1,8 +1,5 @@
 namespace ImagingPipeline.Common.Dtos.Rules.Models;
 
-/// <summary>
-/// Known algorithm identifiers used across the imaging pipeline.
-/// </summary>
 public enum AlgorithmName
 {
     FindAir,
